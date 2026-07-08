87bb25098ad3dbb1d4340231ef044898d45c4aa4
Khizar Nawab <khizarnawab@Khizars-MacBook-Pro.local>
2026-07-08 18:03:27 +0500

    fix(db): regenerate correct migration

--- apps/server/src/db/migrations/0011_checklist_category_fk.sql
+++ apps/server/src/db/migrations/0011_checklist_category_fk.sql
@@ -1,3 +1,4 @@
-ALTER TABLE "checklist_items" RENAME COLUMN "category" TO "category_id";--> statement-breakpoint
+ALTER TABLE "checklist_items" ADD COLUMN "category_id" integer;--> statement-breakpoint
 ALTER TABLE "checklist_items" ADD CONSTRAINT "checklist_items_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "checklist_items_category_id_idx" ON "checklist_items" USING btree ("category_id");
+CREATE INDEX "checklist_items_category_id_idx" ON "checklist_items" USING btree ("category_id");--> statement-breakpoint
+ALTER TABLE "checklist_items" DROP COLUMN "category";
